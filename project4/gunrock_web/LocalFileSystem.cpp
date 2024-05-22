2de74bbb6522dd42bd07bdb2d527c055b4affa66
Alp <aguler@ucdavis.edu>
2024-05-22 02:07:54 -0700

    bits.cpp done (not gradescoped)

--- project4/gunrock_web/LocalFileSystem.cpp
+++ project4/gunrock_web/LocalFileSystem.cpp
@@ -2,19 +2,39 @@
 #include <string>
 #include <vector>
 #include <assert.h>
-
+#include <cstring>
 #include "LocalFileSystem.h"
 #include "ufs.h"
 
+#define UFS_BLOCK_SIZE (4096)
+
 using namespace std;
 
+// implement stat and read to implement ds3ls and ds3cat (remember, the root directory is always inode 0 in our file system). 
+// For ds3bits you'll want to implement readSuperBlock, readInodeBitmap, and readDataBitmap.
 
 LocalFileSystem::LocalFileSystem(Disk *disk) {
   this->disk = disk;
 }
 
 void LocalFileSystem::readSuperBlock(super_t *super) {
+  //read the first disk block
+  super_t buffer[4096];
+  disk->readBlock(0, buffer);
+  memcpy(super, buffer, sizeof(super_t));
 
+}
+
+void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap){
+  for (int i = 0; i < super->inode_bitmap_len; i++){ //might need to make i increment by blocksize
+    disk->readBlock(super->inode_bitmap_addr+i, inodeBitmap);
+  }
+  
+}
+void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap){
+  for (int i = 0; i < super->data_bitmap_len; i++){ //might need to make i increment by blocksize
+      disk->readBlock(super->data_bitmap_addr+i, dataBitmap);
+  }
 }
 
 int LocalFileSystem::lookup(int parentInodeNumber, string name) {
@@ -25,7 +45,7 @@ int LocalFileSystem::stat(int inodeNumber, inode_t *inode) {
   return 0;
 }
 
-int LocalFileSystem::read(int inodeNumber, void *buffer, int size) {
+int LocalFileSystem::read(int inodeNumber, void *buffer, int size) { //read contents of a file or dir
   return 0;
 }
 
--- project4/gunrock_web/ds3bits.cpp
+++ project4/gunrock_web/ds3bits.cpp
@@ -9,10 +9,43 @@
 
 using namespace std;
 
-
+//For ds3bits you'll want to implement readSuperBlock, readInodeBitmap, and readDataBitmap.
+// ./ds3bits disk_testing/a.img
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     cout << argv[0] << ": diskImageFile" << endl;
     return 1;
   }
+  string filename = argv[1];
+  Disk disk(filename, 4096);
+
+  LocalFileSystem lfs(&disk);
+
+  super_t super;
+  lfs.readSuperBlock(&super);
+
+  unsigned char inodeBuf[4096*super.inode_bitmap_len];
+  lfs.readInodeBitmap(&super, inodeBuf);
+
+  unsigned char dataBuf[4096*super.data_bitmap_len];
+  lfs.readDataBitmap(&super, dataBuf);
+
+  printf("Super\n");
+  printf("inode_region_addr %d\n", super.inode_region_addr);
+  printf("data_region_addr %d\n", super.data_region_addr);
+  printf("\n");
+  printf("Inode bitmap\n");
+  for (int i = 0; i < super.inode_bitmap_len*4096; i++){
+    cout << (unsigned int) inodeBuf[i] << " ";
+  }
+  printf("\n");
+  printf("\nData bitmap\n");
+  for (int i = 0; i < super.data_bitmap_len*4096; i++){
+    cout << (unsigned int) dataBuf[i] << " ";
+  }
+  printf("\n");
+
+  return 0;
+
+
 }
